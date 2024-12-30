e3be6aae9f281018c6b32aa8052f8c9f1056d230
Lorenzo-alt <lorenzo.juliati@terra.com.br>
2024-12-30 20:20:36 -0300

    chore: teste 5

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,12 @@
 'use server';
-import Conhecimentos from '../components/Conhecimentos';
-import Contato from '../components/Contato';
-import Projetos from '../components/Projetos';
-import { QueryType } from './types';
-import { fetchHygraphQuery } from './utils/getDataHygraph';
 
-const getDataPage = (): Promise<QueryType> => {
-  const query = `
-query MyQuery {
-  home(where: {id: "clzc08cne0dpp07lu0l4lbxfl"}) {
-    sessaoConhecimentos {
-      conhecimentos(first: 30) {
-        ... on Conhecimento {
-          id
-          titulo
-          icone {
-            url
-          }
-        }
-      }
-    }
-    sessaoContato {
-      dados {
-        email
-        telefone
-      }
-    }
-    sessaoProjetos {
-      listaProjetos {
-        ... on Projeto {
-          projetoModel {
-            titulo
-            fichaTecnica(first: 30) {
-              ... on Conhecimento {
-                titulo
-                icone {
-                  url
-                }
-              }
-            }
-            descricao {
-              html
-            }
-            mocks {
-              mockBrowser1 {
-                url
-              }
-              mockBrowser2 {
-                url
-              }
-              mockBrowser3 {
-                url
-              }
-              mockMobile1 {
-                url
-              }
-              mockMobile2 {
-                url
-              }
-            }
-            url
-            urlGit
-            urlLive
-          }
-        }
-      }
-    }
-  }
-}
-`;
-  const dados = fetchHygraphQuery(query, 60);
+import Conhecimentos from '@/components/Conhecimentos';
 
-  // dados.then((resp) => console.log(resp.home.sessaoProjetos));
+import { getDataPage } from './utils/getDataPage';
 
-  return dados as Promise<QueryType>;
-};
+// import Conhecimentos from '../components/Conhecimentos';
+// import Contato from '../components/Contato';
+// import Projetos from '../components/Projetos';
 
 export default async function Home() {
   const resp = await getDataPage();
@@ -82,8 +14,8 @@ export default async function Home() {
   return (
     <section className='flex w-full flex-col items-center gap-5 overflow-y-scroll px-5 py-5 md:px-10 lg:px-0'>
       aaaa
-      {/* <Conhecimentos dados={resp.home.sessaoConhecimentos} />
-      <Projetos dados={resp.home.sessaoProjetos.listaProjetos} />
+      <Conhecimentos dados={resp.home.sessaoConhecimentos} />
+      {/* <Projetos dados={resp.home.sessaoProjetos.listaProjetos} />
       <Contato dados={resp.home.sessaoContato} /> */}
     </section>
     // <div>aaaa</div>
---
+++ app/utils/getDataPage.ts
@@ -0,0 +1,73 @@
+import { QueryType } from '../types';
+import { fetchHygraphQuery } from './getDataHygraph';
+
+export const getDataPage = (): Promise<QueryType> => {
+  const query = `
+query MyQuery {
+  home(where: {id: "clzc08cne0dpp07lu0l4lbxfl"}) {
+    sessaoConhecimentos {
+      conhecimentos(first: 30) {
+        ... on Conhecimento {
+          id
+          titulo
+          icone {
+            url
+          }
+        }
+      }
+    }
+    sessaoContato {
+      dados {
+        email
+        telefone
+      }
+    }
+    sessaoProjetos {
+      listaProjetos {
+        ... on Projeto {
+          projetoModel {
+            titulo
+            fichaTecnica(first: 30) {
+              ... on Conhecimento {
+                titulo
+                icone {
+                  url
+                }
+              }
+            }
+            descricao {
+              html
+            }
+            mocks {
+              mockBrowser1 {
+                url
+              }
+              mockBrowser2 {
+                url
+              }
+              mockBrowser3 {
+                url
+              }
+              mockMobile1 {
+                url
+              }
+              mockMobile2 {
+                url
+              }
+            }
+            url
+            urlGit
+            urlLive
+          }
+        }
+      }
+    }
+  }
+}
+`;
+  const dados = fetchHygraphQuery(query, 60);
+
+  // dados.then((resp) => console.log(resp.home.sessaoProjetos));
+
+  return dados as Promise<QueryType>;
+};
